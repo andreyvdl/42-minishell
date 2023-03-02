b490a059548496a1d58aad625acd43242a1e0380
adantas- <adantas-@student.42sp.org.br>
2023-03-02 18:01:01 -0300

    sh

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:21:39 by adantas-          #+#    #+#             */
-/*   Updated: 2023/03/01 17:58:48 by adantas-         ###   ########.fr       */
+/*   Updated: 2023/03/02 13:58:25 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@ int	main(void)
 {
 	char	*command;
 	size_t	i;
+	char	*user;
 	// int		oquotes;
 
 	command = 0x0;
 	i = 0;
+	user = getenv("USER");
+	printf("\e[1mHello %s, welcome to\n", user);
 	printf("___  ________ _   _ _____ _____ _   _ _____ _     _\n");
 	printf("|  \\/  |_   _| \\ | |_   _/  ___| | | |  ___| |   | |\n");
 	printf("| .  . | | | |  \\| | | | \\ `--.| |_| | |__ | |   | |\n");
 	printf("| |\\/| | | | | . ` | | |  `--. \\  _  |  __|| |   | |\n");
 	printf("| |  | |_| |_| |\\  |_| |_/\\__/ / | | | |___| |___| |____\n");
 	printf("\\_|  |_/\\___/\\_| \\_/\\___/\\____/\\_| |_|____/\\_____|_____/");
-	printf("\n\n");
+	printf("\e[0m\n");
 	while (1)
 	{
-		command = readline("🐚\e[1;32mMINISHELL\e[0m$>");
+		command = readline("\e[1;36m🐚Type your command\e[0m$>");
 		// while (command[i])
 		// {
 		// 	if
